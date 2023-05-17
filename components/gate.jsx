b078c8ae690075889cc3ae397000e20a09b5593d
Angela Gilhotra <angelagilhotra@gmail.com>
2023-05-17 03:57:49 -0300

    helpful comments

--- components/gate.jsx
+++ components/gate.jsx
@@ -1,18 +1,17 @@
-import { useState, useEffect } from "react";
 import styles from "../styles/Gate.module.css";
 import useLockedState from "../hooks/useLockedState";
 
 const Gate = ({
   contractAddresses,
   walletAddress,
-  strategy, // "any", "all", ""
+  strategy, // "any", "all"
   chain,
   children
 }) => {
   const { isLocked } = useLockedState({
     contractAddresses,
     walletAddress,
-    strategy, // "any", "all", ""
+    strategy, // "any", "all"
     chain
   });
 
